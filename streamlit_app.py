796c3a500ee47cea3637ff1cb7fff49e8968d055
Meghana Gundeboina <124271236+meghana010@users.noreply.github.com>
2024-03-25 18:03:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas
 import requests
 from snowflake.snowpark.functions import col
 
@@ -16,6 +17,9 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 ingredients_list=st.multiselect('Choose upto 5 ingredients: ',my_dataframe,max_selections=5)
 
 if ingredients_list:
@@ -23,7 +27,9 @@ if ingredients_list:
     for each_fruit in ingredients_list:
 	    ingredients_string+=each_fruit+' '
 	    st.subheader(each_fruit+' Nutrition Information')
-	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+each_fruit)
+	    search_on=pd_df.loc[pd_df['FRUIT_NAME']==each_fruit,'SEARCH_ON'].iloc[0]
+	    st.write('The search value for ',each_fruit,'is',search_on,'.')
+	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
 	    fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
