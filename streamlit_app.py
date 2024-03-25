c88fdc20aabf68c6f2796531a11fc8e49b006d65
Meghana Gundeboina <124271236+meghana010@users.noreply.github.com>
2024-03-25 17:19:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,9 +12,6 @@ st.write(
 )
 name_on_order=st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be:',name_on_order)
-#option=st.selectbox('What is your favorite fruit?',('Banana','Strawberries','Peaches'))
-#st.write('Your favorite fruit is:',option)
-#session = get_active_session()
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -25,6 +22,9 @@ if ingredients_list:
     ingredients_string=''
     for each_fruit in ingredients_list:
         ingredients_string+=each_fruit+' '
+	st.subheader(each_fruit+'Nutrition Information')
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+each_fruit)
+	fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string +"""','"""+ name_on_order+"""')"""
@@ -35,9 +35,7 @@ if ingredients_list:
         stmt='Your Smoothie is ordered, '+name_on_order+' !',
 
         st.success(stmt ,icon="✅")
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
+
         
 	    
